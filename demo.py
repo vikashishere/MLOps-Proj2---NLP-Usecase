1f8cadb350e7965f945cc92e625f2f3215a92cb8
Vikash <vikashdas770@gmail.com>
2024-03-25 16:04:44 +0530

    exception module added

--- demo.py
+++ demo.py
@@ -1,2 +1,16 @@
+# Below 2 lines of code are for the testing purpose for logging
+
+# from hate.logger import logging
+# logging.info("Welcome to the project")
+# ----------------------------------------------------------------------
+
+# Below code block is for the testing purpose for exception module
 from hate.logger import logging
-logging.info("Welcome to the project")
+from hate.exception import CustomException
+import sys
+
+
+try:
+    a = "hello"/0
+except Exception as e:
+    raise CustomException(e, sys)
