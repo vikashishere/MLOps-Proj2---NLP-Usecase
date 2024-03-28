2e4c9aa5730907a2355778d8413a0fa11ae92e20
Vikash <vikashdas770@gmail.com>
2024-03-28 20:15:47 +0530

    code updated till model pusher to GCP storage

--- hate/entity/artifact_entity.py
+++ hate/entity/artifact_entity.py
@@ -16,3 +16,12 @@ class ModelTrainerArtifacts:
     trained_model_path:str
     x_test_path: list
     y_test_path: list
+
+
+@dataclass
+class ModelEvaluationArtifacts:
+    is_model_accepted: bool
+
+@dataclass
+class ModelPusherArtifacts:
+    bucket_name: str
